bbf5c8df8c8878d5ea501403c59b88dc68bbe884
krishnamodhavadiya <krishamodha2004@gmail.com>
2026-05-28 11:53:28 +0530

    refactor(blocks-builder): enhance isDeepEqual utility with strict types and correct date comparison

--- src/components/payload/BlocksBuilder/utils/comparison.ts
+++ src/components/payload/BlocksBuilder/utils/comparison.ts
@@ -8,15 +8,18 @@
  * @param b - Second value to compare
  * @returns boolean indicating if the values are deeply equal
  */
-export function isDeepEqual(a: any, b: any): boolean {
+export function isDeepEqual(a: unknown, b: unknown): boolean {
   // If they are strictly equal by reference or value
   if (a === b) {
     return true;
   }
 
   // Handle Date objects
-  if (a instanceof Date && b instanceof Date) {
-    return a.getTime() === b.getTime();
+  if (a instanceof Date || b instanceof Date) {
+    if (a instanceof Date && b instanceof Date) {
+      return a.getTime() === b.getTime();
+    }
+    return false;
   }
 
   // If either value is a primitive or null, and they weren't strictly equal, they are not equal.
@@ -33,11 +36,13 @@ export function isDeepEqual(a: any, b: any): boolean {
   }
 
   if (isArrayA && isArrayB) {
-    if (a.length !== b.length) {
+    const arrA = a as unknown[];
+    const arrB = b as unknown[];
+    if (arrA.length !== arrB.length) {
       return false;
     }
-    for (let i = 0; i < a.length; i++) {
-      if (!isDeepEqual(a[i], b[i])) {
+    for (let i = 0; i < arrA.length; i++) {
+      if (!isDeepEqual(arrA[i], arrB[i])) {
         return false;
       }
     }
@@ -46,8 +51,10 @@ export function isDeepEqual(a: any, b: any): boolean {
 
   // Handle standard objects
   // Filter out structural metadata properties: 'id' and 'blockType'
-  const keysA = Object.keys(a).filter(key => key !== 'id' && key !== 'blockType');
-  const keysB = Object.keys(b).filter(key => key !== 'id' && key !== 'blockType');
+  const objA = a as Record<string, unknown>;
+  const objB = b as Record<string, unknown>;
+  const keysA = Object.keys(objA).filter(key => key !== 'id' && key !== 'blockType');
+  const keysB = Object.keys(objB).filter(key => key !== 'id' && key !== 'blockType');
 
   if (keysA.length !== keysB.length) {
     return false;
@@ -55,13 +62,14 @@ export function isDeepEqual(a: any, b: any): boolean {
 
   // Check that all keys in a are in b and their values are deeply equal
   for (const key of keysA) {
-    if (!Object.prototype.hasOwnProperty.call(b, key)) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key)) {
       return false;
     }
-    if (!isDeepEqual(a[key], b[key])) {
+    if (!isDeepEqual(objA[key], objB[key])) {
       return false;
     }
   }
 
   return true;
 }
+
