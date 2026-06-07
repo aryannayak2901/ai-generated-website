bc692b75632d9886124ca91dcc25b8756e9d874d
aryannayak2901 <tech2aryan2901@gmail.com>
2026-06-07 21:15:26 +0530

    resolve image not uploaded in deployment

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -47,7 +47,7 @@ export default buildConfig({
 
   plugins: [
     vercelBlobStorage({
-      enabled: !!process.env.BLOB_READ_WRITE_TOKEN,
+      enabled: true,
       collections: {
         media: true,
       },
